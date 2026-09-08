d1b5234cc9e09ed46396e2b2a9af523daa23a16c
rvlto974 <jacquet-m@protonmail.com>
2026-09-08 08:28:02 +0400

    Ajout entité Animal avec relation vers Habitat

---
+++ src/Entity/Animal.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AnimalRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AnimalRepository::class)]
+class Animal
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $espece = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTime $dateNaissance = null;
+
+    #[ORM\Column(length: 10, nullable: true)]
+    private ?string $sexe = null;
+
+    #[ORM\ManyToOne(inversedBy: 'animals')]
+    private ?Habitat $habitat = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $etatSante = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getEspece(): ?string
+    {
+        return $this->espece;
+    }
+
+    public function setEspece(string $espece): static
+    {
+        $this->espece = $espece;
+
+        return $this;
+    }
+
+    public function getDateNaissance(): ?\DateTime
+    {
+        return $this->dateNaissance;
+    }
+
+    public function setDateNaissance(?\DateTime $dateNaissance): static
+    {
+        $this->dateNaissance = $dateNaissance;
+
+        return $this;
+    }
+
+    public function getSexe(): ?string
+    {
+        return $this->sexe;
+    }
+
+    public function setSexe(?string $sexe): static
+    {
+        $this->sexe = $sexe;
+
+        return $this;
+    }
+
+    public function getHabitat(): ?Habitat
+    {
+        return $this->habitat;
+    }
+
+    public function setHabitat(?Habitat $habitat): static
+    {
+        $this->habitat = $habitat;
+
+        return $this;
+    }
+
+    public function getEtatSante(): ?string
+    {
+        return $this->etatSante;
+    }
+
+    public function setEtatSante(?string $etatSante): static
+    {
+        $this->etatSante = $etatSante;
+
+        return $this;
+    }
+}
